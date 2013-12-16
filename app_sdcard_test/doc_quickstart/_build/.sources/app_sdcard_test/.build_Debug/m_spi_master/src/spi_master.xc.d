ac48cdf5a7e44814dcd9827c550886e1924eeaa5
Shanthini <shanthini@shanthini-ThinkPad-T430.(none)>
2013-12-16 15:18:32 +0530

    Doc changes implemented

--- app_sdcard_test/doc_quickstart/_build/.sources/app_sdcard_test/.build_Debug/m_spi_master/src/spi_master.xc.d
+++
@@ -1,5 +0,0 @@
-.build_Debug/m_spi_master/src/spi_master.xc.d ./.build_Debug/m_spi_master/src/spi_master.xc.o: \
-  ../../sc_spi/module_spi_master/src/spi_master.xc \
-  ../../sc_spi/module_spi_master/src/spi_master.h \
-  ../../sc_sdcard/module_sdcardSPI/src/spi_conf.h
-
